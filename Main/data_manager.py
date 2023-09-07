dc8634dd585972d07d0dcb76e2cc8bc61269b643
RadioPassive <46291903+Ian-Phyton@users.noreply.github.com>
2023-09-07 09:44:13 +0800

    Update data_manager.py

--- Main/data_manager.py
+++ Main/data_manager.py
@@ -3,7 +3,7 @@
 
 
 
-def insert_data(temperature, sensor_id):
+def write_data(temperature, sensor_id):
     # Write a function that...
     # Compare sensor_id, if found matching id and insert temperature to the respective id
     # Else sensor_id not found in dictionary, add new sensor id and insert temperature
