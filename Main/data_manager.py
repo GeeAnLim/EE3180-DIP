8d59af591d74a916269ca7c5e9c5ea27d16d8954
RadioPassive <46291903+Ian-Phyton@users.noreply.github.com>
2023-09-11 13:23:40 +0800

    Update data_manager.py

--- Main/data_manager.py
+++ Main/data_manager.py
@@ -1,11 +1,12 @@
 # Will run by Raspberry Pi 4
-# Temporarily store sensor_id, temperature pairs in dictionaries
+# Store sensor_id and sensor data into data_manager.csv
+# Historical data is not required, each sensor_id will occupy one row, and subsequent columns are for various sensor data
 
+sensor_id = []
 
-
-def write_data(temperature, sensor_id):
+def write_data(sensor_id, sensor_data1, sensor_data2):
     # Write a function that...
-    # Compare sensor_id, if found matching id and insert temperature to the respective id
-    # Else sensor_id not found in dictionary, add new sensor id and insert temperature
+    # Compare sensor_id, if found matching id and insert sensor data to the respective id
+    # Else sensor_id not found in list, add new sensor id and insert sensor data
     return
 
