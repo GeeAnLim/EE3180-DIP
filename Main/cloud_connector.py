ba0d0b714408dc8d085fe7b32741de6e3be50784
RadioPassive <46291903+Ian-Phyton@users.noreply.github.com>
2023-09-13 22:20:14 +0800

    Update cloud_connector.py

--- Main/cloud_connector.py
+++ Main/cloud_connector.py
@@ -1,9 +1,37 @@
-# Will run by Raspberry Pi 4
-# Connect via SSH, look through the tutorial below:
-# Detailed: https://learn.microsoft.com/en-us/azure/virtual-machines/linux/create-ssh-keys-detailed
-# Quick Guide: https://learn.microsoft.com/en-us/azure/virtual-machines/linux/mac-create-ssh-keys
-# Might need bash scripts instead of python scripts (linux command line scripts)
+import psycopg2
+import pandas as pd
 
 
-# Then, continuously read data from data_manager.csv
-# Write data into database.csv
+def read_csv(filename):
+    # Read csv file and package it into a pandas dataframe
+    # return the dataframe
+    return
+
+
+def get_data(dataframe):
+    # Trim the dataframe, until we are left with the relevant columns:
+    # Datetime, sensor_id/unit no., temperature, acceleration XYZ
+    # return trimmed dataframe
+    return
+
+
+def write_data(dataframe):
+    # SQL instruction to query latest data's datetime
+    # If data's datetime not same with dataframe's datetime
+    # Then we write data into correct location, return True
+    # Else, we return False
+    return
+
+
+connection = psycopg2.connect(user="dbfirenet_user", password="G9Fw38n8WjMfN4zTBydkxYqZFefZSiM4", host="dpg-ck0lntu3ktkc73f98tcg-a.singapore-postgres.render.com", port="5432", database="dbfirenet")
+cursor = connection.cursor()
+
+
+while True:
+    
+    df = get_data(read_csv('data_manager.csv'))
+    
+    if write_data(df):
+        print('New data recorded!')
+    else:
+        print('Waiting for new data...')
