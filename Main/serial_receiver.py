7efc517a707aff845c9d0a4bc85942e785a038d5
PGK321 <144540764+PGK321@users.noreply.github.com>
2023-09-16 00:16:14 +0800

    Update serial_receiver.py

--- Main/serial_receiver.py
+++ Main/serial_receiver.py
@@ -6,15 +6,9 @@ import enocean.utils
 from enocean.communicators.serialcommunicator import SerialCommunicator
 from enocean.protocol.packet import RadioPacket
 from enocean.protocol.constants import PACKET, RORG
-import sys
-import traceback
 import os
 from enocean.protocol.eep import EEP
-from enocean.protocol import crc8
-import xml.dom.minidom as MD
 from datetime import datetime
-import numpy
-import pandas
 import csv
 
 
@@ -26,17 +20,8 @@ except ImportError:
 
 with open('data_manager.csv', 'w') as csv_file:
     csvwriter = csv.writer(csv_file)
-    Column_list = ["Date", "Time", "Address" ,"Sensor ID", "Temperature", "Acceleration X", "Acceleration Y", "Acceleration Z", "Magnet Contact"]
+    Column_list = ["Date", "Time", "Sensor ID", "Temperature", "Acceleration X", "Acceleration Y", "Acceleration Z", "Magnet Contact"]
     csvwriter.writerow(Column_list)
-    csvwriter.writerow('\n')
-
-
-
-def writeline(current_day, current_time, address, sensor_id, TMP, ACX, ACY, ACZ, CO):
-    csvwriter.writerow([current_day, current_time, address, sensor_id, TMP, ACX, ACY, ACZ, CO])
-    csvwriter.writerow('\n')
-    return
-
 
 
 init_logging()
@@ -61,25 +46,9 @@ while communicator.is_alive():
                 packet_id[len(packet_id) - 1] += str(hex(packet_id[i]).split('x')[-1])
             
             sensor_id = packet_id[len(packet_id) - 1]
-            print(sensor_id)                                                         #Display for testing(Success)
+            print(sensor_id)                                #Display for testing(Success)
 
-            if sensor_id == '42261f3':
-                address = "Blk A01 #02-1234, S760A01"
-
-            elif sensor_id == '42262a':
-                address = "Blk B01 #02-1234, S760B01"
-
-            elif sensor_id == '4226076':
-                address = "Blk C01 #02-1234, S760C01"
             
-            elif sensor_id == '4226087':
-                address = "Blk D01 #02-1234, S760D01"
-
-            elif sensor_id == '4226222':
-                address = "Blk E01 #02-1234, S760E01"
-
-            elif sensor_id == '42261EA':
-                address = "Blk F01 #02-1234, S760F01"
 
             current_datetime = datetime.now()
             current_time = current_datetime.strftime("%H:%M:%S")
@@ -93,56 +62,27 @@ while communicator.is_alive():
                 if k == 'TMP':
                     TMPdata = packet.parsed[k]
                     TMP = TMPdata['value']
-                    print(TMP)
                 
                 elif k == 'ACX':
                     ACXdata = packet.parsed[k]
                     ACX = ACXdata['value']
-                    #print(ACX)
                 
                 elif k == 'ACY':
                     ACYdata = packet.parsed[k]
                     ACY = ACYdata['value']
-                    #print(ACY)
                 
                 elif k == 'ACZ':
                     ACZdata = packet.parsed[k]
                     ACZ = ACZdata['value']
-                    #print(ACZ)
 
                 elif k == 'CO':
                     COdata = packet.parsed[k]
                     CO = COdata['value']
-                    #print(CO)
+
+            with open('data_manager.csv', 'w') as csv_file:
+                csvwriter = csv.writer(csv_file)
+                csvwriter.writerow([current_day, current_time, sensor_id, TMP, ACX, ACY, ACZ, CO])
                 
-
-            print(current_time)
-            print(current_day)
-
-            csvwriter.writerow([current_day, current_time, address, sensor_id, TMP, ACX, ACY, ACZ, CO])
-            csvwriter.writerow('\n')
-
-            
-            
-
-
-
-
-                #current_day.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 1)
-                #current_time.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 2)
-                #address.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 3)
-                #sensor_id.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 4)
-                #TMP.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 5)
-                #ACX.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 6)
-                #ACY.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 7)
-                #ACZ.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 8)
-                #CO.to_csv(csv_file, mode = 'a', index = False, header = False, columns = 9)
-            
-            
-            
-
-
-            
 
     except queue.Empty:
         continue
